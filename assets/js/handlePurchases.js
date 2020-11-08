9d73266e733450f028a10c975eaf549e3846544a
Tet <ic3dplasma@gmail.com>
2020-11-07 16:15:29 -0800

    pus

--- assets/js/handlePurchases.js
+++ assets/js/handlePurchases.js
@@ -106,8 +106,8 @@ function login(token) {
 }
 async function yes() {
     var guildid = getUrlVars("code")
-    console.log("yas")
-    if (!localStorage.getItem("DazaiAPIData") && guildid && !guildid[code]) {
+    // console.log("")
+    if (!localStorage.getItem("DazaiAPIData") && guildid && !guildid["code"]) {
         window.location.replace("https://discord.com/api/oauth2/authorize?client_id=747901310749245561&redirect_uri=https%3A%2F%2Fdazai.app%2Fself%2F&response_type=code&scope=identify")
         return;
     }
