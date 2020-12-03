6beccb7b1d387e53c9995a6e6cf2b7d49d39cfd0
Tet <ic3dplasma@gmail.com>
2020-12-03 15:38:03 -0800

    a
    a

--- assets/js/tradeInvHandler.js
+++ assets/js/tradeInvHandler.js
@@ -306,7 +306,7 @@ async function yes() {
         // return;
     }
     httpGet(guildid).then(chans => {
-        if (guilds === "relog"){
+        if (chans === "relog"){
             localStorage.clear()
             return window.location.replace("https://discord.com/api/oauth2/authorize?client_id=747901310749245561&redirect_uri=https%3A%2F%2Fdazai.app%2Finventory%2F&response_type=code&scope=identify%20email%20connections%20guilds")
         } 
