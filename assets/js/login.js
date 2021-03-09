c622f46f4f9aa74c37e25559a4fceb0694e6b3b1
Tet <ic3dplasma@gmail.com>
2021-03-09 00:16:37 -0800

    hehe

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,9 +7,9 @@ function getUrlVars() {
     return vars;
 }
 let urlVars = getUrlVars();
-if (!urlVars[access_token])
+if (!urlVars["access_token"])
 	window.location.href = "https://discord.com/api/oauth2/authorize?client_id=747901310749245561&redirect_uri=https%3A%2F%2Fdazai.app%2Flogin.html&response_type=code&scope=identify%20email";
 else{
-	localStorage.setItem("token",urlVars[access_token]);
+	localStorage.setItem("token",urlVars["access_token"]);
 	window.location.href = "https://dazai.app/user"
 }
